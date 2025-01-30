153cb94b27179c80383684835404464e396211dd
Artem <skickoartem74@gmail.com>
2025-01-30 14:39:02 +0100

    make build

---
+++ index.1f218bb3.js
@@ -0,0 +1,2 @@
+var e=document.getElementsByTagName("div"),t=document.getElementsByTagName("img"),s=parseInt(e[0].style.offsetHeight/2),a=parseInt(e[0].style.offsetWidth/2);t[0].style.position="absolute";var l=parseInt(t[0].style.top);l+=parseInt(s);var n=parseInt(t[0].style.left);n+=parseInt(a),t[0].style.top=l,t[0].style.left=n;
+//# sourceMappingURL=index.1f218bb3.js.map
