7e1a765e1648f04a7310a47e903fe016745974c4
Artem <skickoartem74@gmail.com>
2025-01-30 14:29:20 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,9 +4,12 @@
 const walls = document.getElementsByTagName('div');
 const spiders = document.getElementsByTagName('img');
 
-const wallHeight = walls[0].style.height / 2;
-const wallWidth = walls[0].style.width / 2;
+const wallHeight = parseInt(walls[0].style.height / 2);
+const wallWidth = parseInt(walls[0].style.width / 2);
 
 spiders[0].style.position = 'absolute';
-spiders[0].style.top += wallHeight;
-spiders[0].style.left += wallWidth;
+spiders[0].style.top += parseInt(wallHeight);
+spiders[0].style.left += parseInt(wallWidth);
+
+spiders[0].style.top = wallHeight;
+spiders[0].style.left = wallWidth;
